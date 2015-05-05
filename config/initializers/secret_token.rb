121cfc6aec12099e0205af3c928a81f6bbcd0c32
bengruber <blgruber@gmail.com>
2015-05-05 10:47:12 -0700

    fewd taking over the world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fewd::Application.config.secret_key_base = 'a2f7496b51ad733423cc39cdf143c3fa4095ed51e055de9b5be2b968f635b89fd95f9856a02a4d403dfde61a3c5b29860839ba1876304e055550688bf1285231'
