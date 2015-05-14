18cefab680ca442e5dfe481453e378a92ac84b30
bengruber <blgruber@gmail.com>
2015-05-13 17:02:50 -0700

    big changes to routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
   def home
-    @full_width = true
+    #@full_width = true
+    @moods = Mood.all
+  end
+
+  def move
+    address = request.location
+    @hood = Hood.near([address.longitude, address.latitude], 2)
+    if @hood.count > 0
+      redirect_to @hood.first
+    else
+      redirect_to hood_path(1)
+    end
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,14 @@ Fewd::Application.routes.draw do
   get "pages/about"
   get "pages/contact"
   get "pages/faq"
+  get "pages/move"
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'pages#move'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
