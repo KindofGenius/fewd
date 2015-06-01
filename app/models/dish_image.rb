bc71e64e2d23d68e5055aabb39e5bec7b814787f
bengruber <blgruber@gmail.com>
2015-06-01 12:58:05 +1000

    adding default dish image

--- app/models/dish_image.rb
+++ app/models/dish_image.rb
@@ -1,5 +1,5 @@
 class DishImage < ActiveRecord::Base
   belongs_to :dish
-  has_attached_file :avatar, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/images/dish-default.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
